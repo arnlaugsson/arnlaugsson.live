f61e7b9b0e7679f2adbdde49badf34b0212ef5bb
Skúli Arnlaugsson <arnlaugsson@users.noreply.github.com>
2021-09-14 17:04:40 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>
+        Hi there{" "}
+        <span role="img" aria-label="A waving hand emoji">
+          👋
+        </span>
+      </h1>
+      <h2>My name is Skúli.</h2>
+      <p>
+        This is my live site. By live, I mean just that. It's whatever I am
+        building at any given time.
+      </p>
+      <p>
+        It doesn't have any clear purpose, other than that. Just someplace to
+        put stuff I'm doing or want to do.
+      </p>
+      <p>If you want to know more about me, please check out my other pages:</p>
+      <ul>
+        <li>
+          <a href="https://arnlaugsson.digital">My Polywork site</a>
+        </li>
+        <li>
+          <a href="https://arnlaugsson.wiki">My exposed second brain</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
